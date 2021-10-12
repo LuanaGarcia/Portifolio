e657200be9c4d36912cebdda9cc570f0e4e9a002
Luana <luanaoliveirafss@gmail.com>
2021-10-12 08:59:08 -0300

    :nerd_face:

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
- 
  document.querySelector(".hamburger").addEventListener("click",()=>
-  document.querySelector(".container").classList.toggle("show-menu")
-);
+  document.querySelector(".container").classList.toggle("show-menu"));
